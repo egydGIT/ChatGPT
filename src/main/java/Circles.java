be3226c5ae8e921a2cb30f0a19a9cbcf2f704d5b
eszter <egyd.eszter@gmail.com>
2024-02-18 20:09:36 +0100

    Circles

---
+++ src/main/java/Circles.java
@@ -0,0 +1,13 @@
+// Feladat: Írj egy programot, amely kiírja az első 12 páros számot!
+
+public class Circles {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 12; i+=2) {
+            System.out.println(i);
+        }
+
+    }
+
+}
